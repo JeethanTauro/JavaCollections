a01fbfcf120fa52ad44e8c5094632330ffebc3af
JeethanTauro <tauroshanjeeth@gmail.com>
2025-04-06 16:36:20 +0530

    arraylist done

---
+++ src/ArrayList_tutorial/ArrayList_04.java
@@ -0,0 +1,12 @@
+package ArrayList_tutorial;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayList_04 {
+    public static void main(String[] args) {
+        String[] names = {"Jeethan" , "Rahul" , "Danish"};
+        ArrayList<String> name_list = (ArrayList<String>) Arrays.asList(names);
+
+    }
+}
